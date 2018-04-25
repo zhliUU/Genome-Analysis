c2df88a1545d71cd2d85b44f53b7f0df11d8db10
Zhiwei Li <li120415323@gmail.com>
2018-04-25 15:53:59 +0200

    Update for reference QUAST

--- analyses/code/3_PacBio_AssemblyQC.sh
+++ analyses/code/3_PacBio_AssemblyQC.sh
@@ -13,7 +13,8 @@ module load bioinfo-tools
 module load quast
 
 # Your commands: It wasn't run through bash, but command line directly
-quast.py /home/zhiwei94/Genome-Analysis/analyses/01_PacBio_genome_assembly/durian-pacbio/durian.contigs.fasta
+quast.py /home/zhiwei94/Genome-Analysis/analyses/01_PacBio_genome_assembly/durian-pacbio/durian.contigs.fasta \
+ -R /home/zhiwei94/Genome-Analysis/analyses/01_PacBio_genome_assembly/contig_download/sequence.fasta
 output=/home/zhiwei94/Genome-Analysis/analyses/03_PacBio_AssemblyQC #TODO: locate the data correctly
 touch repoerForPacBioQC
 echo "Running quast..." > repoerForPacBioQC #save to some known file to check easiler

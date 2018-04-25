43ea32189f2dd5faa724c728797893ec562b64b9
Zhiwei Li <li120415323@gmail.com>
2018-04-25 15:12:33 +0200

    Change code

--- analyses/code/4_MappingIllumina.sh
+++ analyses/code/4_MappingIllumina.sh
@@ -2,9 +2,9 @@
 
 #SBATCH -A g2018003
 #SBATCH -p core
-#SBATCH -n 4
-#SBATCH -t 20:00:00
-#SBATCH -J 1_gene_assembly
+#SBATCH -n 2
+#SBATCH -t 02:00:00
+#SBATCH -J 04_MappingIllumina
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user li120415323@gmail.com
 
@@ -12,4 +12,4 @@
 module load bioinfo-tools
 module load bwa
 # check if works with compressed data
-bwa mem /home/zhiwei94/Genome-Analysis/analyses/01_PacBio_genome_assembly/durian-pacbio/SRR6058604_scaffold_11.1P.fastq.gz /home/zhiwei94/Genome-Analysis/analyses/data/WGS_trimmed_data/read1.fq /home/zhiwei94/Genome-Analysis/analyses/data/WGS_trimmed_data/SRR6058604_scaffold_11.1P.fastq.gz > /home/zhiwei94/Genome-Analysis/analyses/03_Mapping_PacBio_Assembly_Illumina/aln-pe.sam
+bwa mem /home/zhiwei94/Genome-Analysis/analyses/01_PacBio_genome_assembly/durian-pacbio/SRR6058604_scaffold_11.1P.fastq.gz /home/zhiwei94/Genome-Analysis/analyses/data/WGS_trimmed_data/SRR6058604_scaffold_11.2P.fastq.gz > /home/zhiwei94/Genome-Analysis/analyses/04_Mapping_PacBio_Assembly_Illumina/aln-pe.sam

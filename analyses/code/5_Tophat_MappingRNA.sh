1355592b9f713c6d9580fb4ccbac0e552f19bb2c
Zhiwei Li <li120415323@gmail.com>
2018-04-27 15:50:30 +0200

    Auto stash before merge of "master" and "origin/master"

--- analyses/code/5_Tophat_MappingRNA.sh
+++ analyses/code/5_Tophat_MappingRNA.sh
@@ -15,8 +15,8 @@ module load tophat
 forword=$1
 reverse=$2
 
-tophat /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/durio_zibe/ \
--o /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA \
+tophat -o /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA \
 -p 2 \
+/home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/durio_zibe/05_Tophat_Mapping_RNA \
 $forword $reverse
 done

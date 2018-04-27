b8aeaf7bf28780f6ff4b95a0406b62208a860f94
Zhiwei Li <li120415323@gmail.com>
2018-04-27 15:10:24 +0200

    edit time, threads=2

--- analyses/code/5_Tophat_MappingRNA.sh
+++ analyses/code/5_Tophat_MappingRNA.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2018003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:45:00 #too long?
+#SBATCH -t 10:00:00 #too long?
 #SBATCH -J 5_Tophat_Mapping_test
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user li120415323@gmail.com
@@ -15,4 +15,8 @@ module load tophat
 forword=$1
 reverse=$2
 
-tophat 
+tophat /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/durio_zibe/05_Tophat_Mapping_RNA \
+-o /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA \
+-p 2 \
+$forword $reverse
+done

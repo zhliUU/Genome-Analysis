b145bd70ee10114f3fe1349d9c95639ed85df47e
Zhiwei Li <li120415323@gmail.com>
2018-04-27 16:18:08 +0200

    Add job ID

--- analyses/code/5_Tophat_MappingRNA.sh
+++ analyses/code/5_Tophat_MappingRNA.sh
@@ -14,8 +14,9 @@ module load tophat
 
 forword=$1
 reverse=$2
+SAMPLE_ID=$3
 
-tophat -o /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA \
+tophat -o /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/sample${SAMPLE_ID} \
 -p 2 \
 /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/durio_zibe/05_Tophat_Mapping_RNA \
 $forword $reverse

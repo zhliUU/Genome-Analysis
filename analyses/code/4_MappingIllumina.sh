ea641242c19fbc524fbc865b08cafd971a729b37
Zhiwei Li <li120415323@gmail.com>
2018-04-28 21:36:00 +0200

    Add bwa index
    if no index will error

--- analyses/code/4_MappingIllumina.sh
+++ analyses/code/4_MappingIllumina.sh
@@ -13,6 +13,7 @@ module load bioinfo-tools
 module load bwa
 #TODO: create pipeline to convert sam to bam file as input for Pilon.
 # check if works with compressed data; check Input: bwa mem ref.fa read1.fq read2.fq > aln-pe.sam
+bwa index /home/zhiwei94/Genome-Analysis/analyses/01_PacBio_genome_assembly/durian-pacbio/durian.contigs.fasta
 bwa mem /home/zhiwei94/Genome-Analysis/analyses/01_PacBio_genome_assembly/durian-pacbio/durian.contigs.fasta \
  /home/zhiwei94/Genome-Analysis/analyses/data/WGS_trimmed_data/SRR6058604_scaffold_11.1P.fastq.gz \
 /home/zhiwei94/Genome-Analysis/analyses/data/WGS_trimmed_data/SRR6058604_scaffold_11.2P.fastq.gz \

79faee9965e248d77117d6b517f3b3182e9a12b4
Zhiwei Li <li120415323@gmail.com>
2018-05-04 13:54:17 +0200

    Run individually

---
+++ analyses/code/5_codeRanIndividually.sh
@@ -0,0 +1,5 @@
+tophat -o /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/sampleFor97 \
+-p 2 \
+/home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/durio_zibe/05_Tophat_Mapping_RNA \
+/home/zhiwei94/Genome-Analysis/analyses/data/RNA_trimmed_data/SRR6040097_scaffold_11.1.fastq.gz \
+/home/zhiwei94/Genome-Analysis/analyses/data/RNA_trimmed_data/SRR6040097_scaffold_11.2.fastq.gz

c1eab34240b257d87b1bbef03b157722e3f40b0a
Zhiwei Li <li120415323@gmail.com>
2018-05-04 16:19:16 +0200

    Change runtime to 1h

--- analyses/code/5_tophatFolder/5_for6069.sh
+++ analyses/code/5_tophatFolder/5_for6069.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2018003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 10:00:00
+#SBATCH -t 1:00:00
 #SBATCH -J 5_Tophat69
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user li120415323@gmail.com

89f83068a2a8726e54b7272ae395eaeb0969f43a
Zhiwei Li <li120415323@gmail.com>
2018-05-04 14:02:34 +0200

    add job

--- analyses/code/5_codeRanIndividually.sh
+++ analyses/code/5_codeRanIndividually.sh
@@ -2,4 +2,14 @@ tophat -o /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/sampleFo
 -p 2 \
 /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/durio_zibe/05_Tophat_Mapping_RNA \
 /home/zhiwei94/Genome-Analysis/analyses/data/RNA_trimmed_data/SRR6040097_scaffold_11.1.fastq.gz \
-/home/zhiwei94/Genome-Analysis/analyses/data/RNA_trimmed_data/SRR6040097_scaffold_11.2.fastq.gz
+/home/zhiwei94/Genome-Analysis/analyses/data/RNA_trimmed_data/SRR6040097_scaffold_11.2.fastq.gz\
+
+tophat -o /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/sampleFor92 \
+-p 2 \
+/home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/durio_zibe/05_Tophat_Mapping_RNA \
+/home/zhiwei94/Genome-Analysis/analyses/data/RNA_trimmed_data/SRR6040092_scaffold_11.1.fastq.gz \
+/home/zhiwei94/Genome-Analysis/analyses/data/RNA_trimmed_data/SRR6040092_scaffold_11.2.fastq.gz\
+
+sbatch 5_Tophat_MappingRNA.sh /home/zhiwei94/Genome-Analysis/analyses/data/RNA_trimmed_data/SRR6040092_scaffold_11.1.fastq.gz \
+/home/zhiwei94/Genome-Analysis/analyses/data/RNA_trimmed_data/SRR6040092_scaffold_11.2.fastq.gz\
+for92

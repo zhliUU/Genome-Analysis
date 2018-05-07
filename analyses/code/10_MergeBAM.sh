029d2f3dbe4ade8c8a5aa214d2c7730306afa4b9
Zhiwei Li <li120415323@gmail.com>
2018-05-07 21:50:38 +0200

    merge, sort and index BAM files

---
+++ analyses/code/10_MergeBAM.sh
@@ -0,0 +1,16 @@
+
+
+#using 40095 40092 40093 40096, excluded: Monthong
+#load samtools
+module load samtools
+#run concatenation
+#it was run in the analyses directory, path to output not clear.
+samtools merge 10_MergeBAM/out.bam \
+ /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/TophatSample40095TrimmedRAW/accepted_hits.bam \
+ /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/TophatSample40092/accepted_hits.bam \
+ /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/TophatSample40093/accepted_hits.bam \
+ /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/TophatSample40096/accepted_hits.bam
+
+#TODO: sort the BAM and index?
+samtools sort out.bam > sorted-out.bam
+samtools index sorted-out.bam

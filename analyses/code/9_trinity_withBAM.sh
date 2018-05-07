1eeb0437a08897f93ca5f7f9d042dca9042a5933
Zhiwei Li <li120415323@gmail.com>
2018-05-07 11:12:28 +0200

    TODO: concatenated BAM input

--- analyses/code/9_trinity_withBAM.sh
+++ analyses/code/9_trinity_withBAM.sh
@@ -12,6 +12,7 @@
 module load bioinfo-tools
 module load trinity
 #locat the bam correctly?
+#change to the concatenated BAM file
 Trinity --seqType fq --max_memory 50G \
 --genome_guided_bam /home/zhiwei94/Genome-Analysis/analyses/05_Tophat_Mapping_RNA/sampleRNA_rawTrimmed_SRR6040095_out/accepted_hits.bam \
 --genome_guided_max_intron 10000 \

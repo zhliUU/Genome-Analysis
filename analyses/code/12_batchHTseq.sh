5ee3bb30a4458af87eb797aa215dd72cf76656d5
Zhiwei Li <li120415323@gmail.com>
2018-05-16 17:33:16 +0200

    Add -t to CDS

--- analyses/code/12_batchHTseq.sh
+++ analyses/code/12_batchHTseq.sh
@@ -8,7 +8,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user li120415323@gmail.com
 
-python -m HTSeq.scripts.count -f bam -r name -i ID \
+python -m HTSeq.scripts.count -f bam -r name -t CDS -i ID \
 /home/zhiwei94/Genome-Analysis/analyses/10_MergeBAM/ALLmerge/sorted-ALLout.bam \
 /home/zhiwei94/Genome-Analysis/analyses/11_Output/test.gff\
 > /home/zhiwei94/Genome-Analysis/analyses/12_HTseq/counts.file

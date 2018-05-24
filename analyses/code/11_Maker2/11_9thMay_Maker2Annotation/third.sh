eeeef8ca08aad07376c6c647c78475f07f6cd4b6
Zhiwei Li <li120415323@gmail.com>
2018-05-24 11:25:13 +0200

    Move code and add comment

---
+++ analyses/code/11_Maker2/11_9thMay_Maker2Annotation/third.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 8
+#SBATCH -t 30:00:00
+#SBATCH -J 11_Maker
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user li120415323@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load maker/3.01.1-beta
+
+maker
---
+++ analyses/code/14_IGV_Pre_Processing.sh
@@ -0,0 +1,13 @@
+module load bioinfo-tools
+module load samtools
+
+samtools sort accepted_hits.bam > sorted-out40093.bam
+samtools index sorted-out40093.bam
+92 93 94 95 96 97
+66 67 69
+
+
+49.8-mRNA-1
+48.1
+48.8
+208.1-mRNA-1
